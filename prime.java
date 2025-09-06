b1288003d9ae324797e6cdb775f87e635f07382e
Navyameruva1 <navyameruva1@gmail.com>
2025-09-06 21:18:03 +0530

    Add files via upload

---
+++ prime.java
@@ -0,0 +1,23 @@
+public class prime {
+
+    public static void main(String[] args) {
+        System.out.println("Prime numbers from 1 to 100 are:");
+        for (int i = 1; i <= 100; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+       public static boolean isPrime(int num) {
+          if (num < 2) {
+            return false;
+        }
+                for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                return false; // Not a prime number
+            }
+        }
+        return true; 
+    }
+}
